3d745ffaa0f2a5144dd020bcae63969c1fad0a45
Maciej-Kordek <jubsgeg@gmail.com>
2024-10-20 14:20:00 +0200

    Letter Capitalization
    Updated node names in XML parser to start with upper case to reflect how they are saved to XML file

--- Assets/XMLParser.cs
+++ Assets/XMLParser.cs
@@ -20,25 +20,25 @@ class XMLParser
 		XDocument xmlDoc = XDocument.Load(_xmlFilePath);
 
 		//Foreach node
-		foreach (var node in xmlDoc.Descendants("node"))
+		foreach (var node in xmlDoc.Descendants("Node"))
 		{
 			List<NodeConnection> nodeConnections = new List<NodeConnection>();
 
 			//Foreach node connection
-			foreach (var nodeConnection in node.Descendants("nodeConnection"))
+			foreach (var nodeConnection in node.Descendants("NodeConnection"))
 			{
-				string to = nodeConnection.Element((XName)"to").Value.ToString().Trim();
+				string to = nodeConnection.Element((XName)"To").Value.ToString().Trim();
 
 				//Relative position of connected node 
-				float x = float.Parse(nodeConnection.Element((XName)"relative_x").Value);
-				float y = float.Parse(nodeConnection.Element((XName)"relative_y").Value);
-				float z = float.Parse(nodeConnection.Element((XName)"relative_z").Value);
+				float x = float.Parse(nodeConnection.Element((XName)"Relative_x").Value);
+				float y = float.Parse(nodeConnection.Element((XName)"Relative_y").Value);
+				float z = float.Parse(nodeConnection.Element((XName)"Relative_z").Value);
 				Vector3 relativePosition = new Vector3(x, y, z);
 
 				nodeConnections.Add(new NodeConnection(to, relativePosition));
 			}
 
-			string name = node.Element((XName)"name").Value.ToString().Trim();
+			string name = node.Element((XName)"Name").Value.ToString().Trim();
 
 			//Add node to node list
 			NodeList.Add(new Node(name, nodeConnections));
