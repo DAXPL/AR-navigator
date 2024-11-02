775e990490f87155678b00dc5fd3c015756a97d4
Miłosz Klim <klim.milosz@gmail.com>
2024-11-02 18:14:40 +0100

    Debug draw

--- Assets/NavigationManager.cs
+++ Assets/NavigationManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -13,52 +14,43 @@ public class NavigationManager : MonoBehaviour
         Debug.Log(dataPath);
         xmlParser = new XMLParser();
         xmlParser.ParseXML(dataPath);
-
-        foreach (Node n in xmlParser.NodeList)
-        {
-            Debug.Log($"Found {n.Name}");
-        }
-
     }
     public void OnImageChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
     {
-        foreach(var image in eventArgs.updated)
+        DrawMesh(eventArgs);
+    }
+
+    private void DrawMesh(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
     {
+        foreach (var image in eventArgs.updated)
+        {
             if (image.trackingState != UnityEngine.XR.ARSubsystems.TrackingState.Tracking) continue;
-            Debug.Log($"Found {image.referenceImage.name}");
+
             // ZnajdŸ aktualny wêze³ na podstawie nazwy obrazu
             Node currentNode = xmlParser.NodeList.FirstOrDefault(n => n.Name == image.referenceImage.name);
             if (currentNode == null) continue;
 
-            // Pozycja wêz³a
+            // Pozycja g³ównego wêz³a
             Vector3 currentPosition = image.transform.position;
 
             // PrzejdŸ przez po³¹czone wêz³y
             foreach (NodeConnection connection in currentNode.ConnectedNodes)
             {
-                // ZnajdŸ po³¹czony wêze³ w liœcie NodeList
-                Node connectedNode = xmlParser.NodeList.FirstOrDefault(n => n.Name == connection.To);
-                if (connectedNode == null) continue;
-
-                // Oblicz wzglêdn¹ pozycjê w przestrzeni AR
+                // Oblicz wzglêdn¹ pozycjê po³¹czonego wêz³a (podwêz³a) w przestrzeni AR
                 Vector3 relativePosition = new Vector3(connection.Relative_x, connection.Relative_y, connection.Relative_z);
                 Vector3 connectedPosition = currentPosition + relativePosition;
 
-                // Wybierz kolor w zale¿noœci od typu po³¹czenia
-                Color lineColor = connectedNode.ConnectedNodes.Any(c => c.To == currentNode.Name) ? Color.red : Color.yellow;
+                // SprawdŸ, czy po³¹czony wêze³ istnieje w `NodeList` jako g³ówny wêze³
+                bool isMainNode = xmlParser.NodeList.Any(n => n.Name == connection.To);
+
+                // Wybierz kolor linii:
+                // - Czerwona, jeœli po³¹czony wêze³ to g³ówny wêze³
+                // - ¯ó³ta, jeœli to podwêze³
+                Color lineColor = isMainNode ? Color.red : Color.yellow;
 
                 // Narysuj liniê od currentPosition do connectedPosition
-                Debug.DrawRay(currentPosition, connectedPosition - currentPosition, lineColor, 0.5f);
+                Debug.DrawRay(currentPosition, connectedPosition - currentPosition, lineColor, 1f);
             }
         }
-    }
-
-    private bool IsNodeInXML(XMLParser parser,string nodeName)
-    {
-        foreach (Node n in parser.NodeList)
-        {
-            if(n.Name == nodeName) return true;
-        }
-        return false;
     }
 }
